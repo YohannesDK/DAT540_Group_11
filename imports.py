a164ce9a562e76b583be4b903f3b87e2b0bc8f47
YohannesDK <kassaye85@gmail.com>
2021-11-26 00:03:42 +0100

    aadded common sense baseline

---
+++ imports.py
@@ -0,0 +1,23 @@
+if __name__ == "__main__":
+    import os
+    import sys
+
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+
+    import seaborn as sns
+    from sklearn.model_selection import train_test_split
+    import numpy as np # linear algebra
+    import pandas as pd
+    import datetime as dt
+
+    from pandas.plotting import scatter_matrix
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LinearRegression
+    from sklearn.ensemble import RandomForestRegressor
+    from sklearn.preprocessing import StandardScaler
+
+    from sklearn.ensemble import RandomForestRegressor
+    from scipy import stats
+
+    print("Import Done")
